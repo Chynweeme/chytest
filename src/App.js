10bc361ea4969ce2a2ba8a1e4d2b10445ad2cef1
Chynwe <echinwenduvictoriae@gmail.com>
2023-01-08 21:38:51 +0100

    5678

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import './App.css';
 import { Routes, Route, Outlet } from "react-router-dom";
-// import Navbar from './Components/Navabar/Navbar.jsx';
+import Navbar from './Components/Navabar/Navbar.jsx';
 import Adults from './Pages/Adults/Adults.jsx';
 // import Choosemode from './Pages/Choosemode/Choosemode';
 import { Homepage } from './Pages/Homepage/Homepage.jsx';
 import RegisterChildren from './Pages/RegisterChildren/RegisterChildren.jsx';
 
-// const Layout = () => (
-//   <>
-//    <Navbar />
-//   <Outlet />
-//   </>
-// )
+const Layout = () => (
+  <>
+   <Navbar />
+  <Outlet />
+  </>
+)
 
 
 function App() {
@@ -20,21 +20,15 @@ function App() {
   return (
     <div className='App'>
     <Routes>
-      {/* <Route element={<Layout />}> */}
+      <Route element={<Layout />}>
 
           <Route path="/" element={<Homepage />}></Route>
           <Route path="/home" element={<Homepage />}></Route>
           <Route path="/home/adultreg" element={<Adults />}></Route>
           <Route path="/home/childreg" element={<RegisterChildren />}></Route>
 
-      {/* <div> */}
-        
-        {/* <Homepage /> */}
-        {/* <Choosemode /> */}
-        {/* <Adults /> */}
-        {/* <RegisterChildren /> */}
-        {/* </div> */}
-      {/* </Route> */}
+      
+      </Route>
     </Routes>
     </div>
   );
